ac201784fd1a4c0fb7e3207d928efb37542cc886
Mederbelines <126400178+Mederbelines@users.noreply.github.com>
2024-04-06 18:11:35 +0200

    add method load_volunteers to CreateDb.py

--- CreateDb.py
+++ CreateDb.py
@@ -1,9 +1,10 @@
 import sqlite3
 import json
-from werkzeug.security import generate_password_hash
 
 
-JSONFILENAME = 'users.json'
+
+JSONFILENAMEUSER = 'users.json'
+JSONFILENAMEVOLUNTEER = 'volunteer.json'
 DBFILENAME = 'Data.sqlite'
 
 def db_run(query, args=(),db_name=DBFILENAME):
@@ -12,7 +13,7 @@ def db_run(query, args=(),db_name=DBFILENAME):
     conn.execute
    
 
-def load(fname=JSONFILENAME, db_name=DBFILENAME):
+def load_users(fname=JSONFILENAMEUSER, db_name=DBFILENAME):
   # possible improvement: do whole thing as a single transaction
   db_run('DROP TABLE IF EXISTS user')
   db_run('DROP TABLE IF EXISTS volunteer')
@@ -20,12 +21,9 @@ def load(fname=JSONFILENAME, db_name=DBFILENAME):
   db_run('DROP TABLE IF EXISTS project_registration')
   db_run('DROP TABLE IF EXISTS project')
 
+  #la table user 
   db_run('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT, registration_date TEXT)')
- 
   insert1 = 'INSERT INTO user VALUES (:id,:username, :password, :email, :registration_date)'
-  
-
-
   with open('users.json', 'r') as fh:
      users = json.load(fh)
   for id, user in enumerate(users):
@@ -33,14 +31,41 @@ def load(fname=JSONFILENAME, db_name=DBFILENAME):
     db_run(insert1, user)
   
 
-def add_user(username,password,email):
-  insert = 'INSERT INTO user (username,password,email) VALUES (?, ?, ?)'
-  password_hash=generate_password_hash(password)
-  db_run(insert,(username,password_hash,email))
 
-load()
-add_user("sarah","12345",'sarah@exemple.com')
-add_user("ines","hamiche",'ines@gmail.com')
-add_user("souso","azerty",'soso@gmail.com')
+def load_volunteers(fname=JSONFILENAMEVOLUNTEER, db_name=DBFILENAME):
+    # Supprimer la table volunteer s'il existe déjà
+    db_run('DROP TABLE IF EXISTS volunteer')
+
+    # Créer la table volunteer
+    db_run('''CREATE TABLE volunteer (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 user_id INTEGER,
+                 full_name TEXT,
+                 date_of_birth TEXT,
+                 address TEXT,
+                 skills TEXT,
+                 phone_number TEXT,
+                 sexe TEXT,
+                 interests TEXT
+              )''')
+
+    
+    insert_query = 'INSERT INTO volunteer (user_id, full_name, date_of_birth, address, skills, phone_number, sexe, interests) \
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+
+    with open(fname, 'r') as fh:
+        volunteers = json.load(fh)
+
+    # Insérer les données des bénévoles dans la table volunteer
+    for volunteer in volunteers:
+        # Convertir la liste de compétences en une chaîne de caractères
+        skills_str = ', '.join(volunteer['skills'])
+        volunteer['skills'] = skills_str
+        db_run(insert_query, volunteer)
+
+  
+load_volunteers()
+
+
 
 
